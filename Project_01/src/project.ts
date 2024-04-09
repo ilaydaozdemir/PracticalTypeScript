f3c7b3685a827cce8ab19cabab07364f9f060c9d
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-04-09 11:21:17 +0300

    Union Type

--- Project_01/src/project.ts
+++ Project_01/src/project.ts
@@ -1,17 +1,2 @@
-let text1: string = "hello";
-text1 = "Merhaba";
-text1 = text1.toLocaleUpperCase();
-
-let text2: string = "HOSGELDINIZ";
-text2 = text2.toLocaleLowerCase();
-
-console.log(text1);
-console.log(text2);
-
-let age: number = 25;
-age = age + 3;
-
-let isAdult: boolean = age >= 18;
-console.log(isAdult);
-isAdult = !isAdult;
-console.log(isAdult);
+let reqStatus: "pending" | "success" | "error" = "pending";
+reqStatus = "error";
