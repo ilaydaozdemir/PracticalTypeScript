ad8efa3660c8711445063c5b96995bf7425b5665
ozdemirilayda <ozdemirilayda95@gmail.com>
2024-04-09 11:46:01 +0300

    Union Type-1

--- Project_01/src/project.ts
+++ Project_01/src/project.ts
@@ -1,2 +1,12 @@
-let reqStatus: "pending" | "success" | "error" = "pending";
-reqStatus = "error";
+let books = ["1984", "Brave New World"];
+/*let foundBook; // type any */
+let foundBook: string | undefined;
+
+for (let book of books) {
+  if (book === "1983") {
+    foundBook = book;
+    foundBook = foundBook.toUpperCase();
+    break;
+  }
+}
+console.log(foundBook?.length);
